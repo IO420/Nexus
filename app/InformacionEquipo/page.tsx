d1a8c0836d46c92e3076fd726ef9741e36656152
CarlosssFlores <307100636@pcpuma.acatlan.unam.mx>
2025-09-08 11:11:33 -0600

    agregue nuevas vistas

--- app/InformacionEquipo/page.tsx
+++ app/InformacionEquipo/page.tsx
@@ -27,7 +27,7 @@ export default function Page() {
                   type="text"
                   //value={user}
                   //onChange={(e) => setUser(e.target.value)}
-                  placeholder="Coloca un número de cuenta..."
+                  placeholder="Coloca"
                 />
 
                 <label className="label">Nombre</label>
