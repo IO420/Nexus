33060b5dbba7709ce4a18ccc811a12edbf71582b
IO420 <320154041@pcpuma.acatlan.unam.mx>
2025-09-19 11:26:00 -0600

    added functionality to button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,28 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
+  const router = useRouter();
+
+  const handleButton = () => {
+    router.push("/Impresiones");
+  };
+
   return (
     <section>
       <img
         src="/404-bg.png" // 👈 pon tu imagen en /public/404-bg.png
         alt="Página no encontrada"
-        className="w-[150px] md:w-[200px] mb-8"
       />
 
-      {/* Título */}
       <h1>404 - Página no encontrada</h1>
 
-      {/* Texto */}
       <p>Lo sentimos, la página que buscas no existe.</p>
       <p>Por favor, verifica la URL o regresa a la página principal.</p>
-      <button className="button buttonSearch"> regresa al inicio</button>
+      <button className="button buttonSearch" onClick={handleButton}>
+        regresa al inicio
+      </button>
     </section>
   );
 }
