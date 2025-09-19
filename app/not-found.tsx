0e546a4cd7478299b22e2e42f6f742ef603f25a0
CarlosssFlores <307100636@pcpuma.acatlan.unam.mx>
2025-09-19 13:01:23 -0600

    new view

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,16 +4,16 @@ export default function NotFound() {
       <img
         src="/404-bg.png" // 👈 pon tu imagen en /public/404-bg.png
         alt="Página no encontrada"
-        className="w-[150px] md:w-[200px] mb-8"
       />
-
-      {/* Título */}
       <h1>404 - Página no encontrada</h1>
 
-      {/* Texto */}
-      <p>Lo sentimos, la página que buscas no existe.</p>
-      <p>Por favor, verifica la URL o regresa a la página principal.</p>
-      <button className="button buttonSearch"> regresa al inicio</button>
+      <p className="text-gray-700 text-xl mb-2">
+        Lo sentimos, la página que buscas no existe
+      </p>
+      <p className="text-gray-700 text-xl">
+        Por favor, verifica la URL o regresa a la página principal.
+      </p>
+      <button> Regresa al inicio</button>
     </section>
   );
 }
