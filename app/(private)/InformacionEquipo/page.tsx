5a81af78721349c9789f59cf748f4608dd998cae
CarlosssFlores <307100636@pcpuma.acatlan.unam.mx>
2025-09-23 14:13:23 -0600

    nuevas vistas y componentes

--- app/(private)/InformacionEquipo/page.tsx
+++ app/(private)/InformacionEquipo/page.tsx
@@ -2,6 +2,7 @@ import ProgramSelector from "@/app/Components/ProgramSelector/ProgramSelector";
 import Toggle from "@/app/Components/Toggle/Toggle";
 
 import "./informacionequipo.css";
+import Equipos from "@/app/Components/Equipos/equipos";
 
 export default function Page() {
   return (
@@ -14,51 +15,16 @@ export default function Page() {
           {
             key: "1",
             label: "Equipos",
-            content: (
-              <form className="containerForm">
-                <div className="groupInput">
-                  <input type="text" />
-                  <button className="button buttonSearch">Buscar</button>
-                </div>
-                <label className="label">Ubicacion</label>
-                <input type="text" placeholder="Coloca" />
-
-                <label className="label">Nombre</label>
-                <input type="text" placeholder="Coloca " />
-
-                <label className="label">Plataforma</label>
-                <select>
-                  <option value="0">Elige</option>
-                  <option value="1">2</option>
-                  <option value="2">3</option>
-                  <option value="3">4</option>
-                  <option value="4">5</option>
-                  <option value="5">6</option>
-                </select>
-
-                <label className="label">Area Ubicacion</label>
-                <select>
-                  <option value="0">Elige</option>
-                  <option value="1">mmmmm</option>
-                  <option value="2">mmmmm</option>
-                  <option value="3">mmmmm</option>
-                  <option value="4">mmmmm</option>
-                  <option value="5">mmmmm</option>
-                </select>
-                <div className="containerButton">
-                  <button className="button buttonSearch">Nuevo</button>
-                  <button className="button buttonSearch">Editar</button>
-                </div>
-              </form>
-            ),
+            content: <Equipos key={2} />,
           },
           {
             key: "2",
             label: "Programa por equipo",
             content: (
               <ProgramSelector
+                key={1}
                 titulo="Programas de Equipo"
-                opcion={["Equipo 1", "Equipo 2", "Equipo 3"]}
+                opcion={["", "Equipo 1", "Equipo 2", "Equipo 3", "Equipo 4"]}
               />
             ),
           },
@@ -67,8 +33,9 @@ export default function Page() {
             label: "Programa por sala",
             content: (
               <ProgramSelector
+                key={2}
                 titulo="Programas por Sala"
-                opcion={["Sala A", "Sala B", "Sala C"]}
+                opcion={["", "PCNET1", "PCNET2", "PCNET3", "PCNET4", "PCNET4"]}
               />
             ),
           },
